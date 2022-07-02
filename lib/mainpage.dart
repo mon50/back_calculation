c805bc3a0ea40f9ff954c76340a6ac06c5199dfc
prog4koka <kazumichi.oka@gmail.com>
2022-07-02 22:20:36 +0900

    hello

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -1,2 +1,20 @@
-//hello
-//changes
+import 'package:flutter/material.dart';
+
+class mainPage extends StatefulWidget {
+
+  @override
+  State<mainPage> createState() => _mainPageState();
+}
+
+class _mainPageState extends State<mainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('seethrough'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded()
+
