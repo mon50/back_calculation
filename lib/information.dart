16040cd01687b992e3a4f5616ea5e26985655091
mikanman <kosp200@gmail.com>
2022-07-03 08:41:46 +0900

    学科の追加

--- lib/information.dart
+++ lib/information.dart
@@ -8,6 +8,12 @@ class InformationPage extends StatefulWidget {
   State<InformationPage> createState() => _InformationPageState();
 }
 
+
+bool button_1 = false;
+bool button_2 = false;
+bool button_3 = false;
+bool button_4 = false;
+
 final List<Map<String, dynamic>> _items = [
   {
     'value': '0',
@@ -35,6 +41,92 @@ final List<Map<String, dynamic>> _items = [
   },
 ];
 
+final List<Map<String, dynamic>> _items_2 = [
+  {
+    'value': '0',
+    'label': '政治経済学部 ',
+  },
+  {
+    'value': '1',
+    'label': '法学部',
+  },
+  {
+    'value': '2',
+    'label': '教育学部',
+  },
+  {
+    'value': '3',
+    'label': '商学部',
+  },
+  {
+    'value': '4',
+    'label': '社会科学部',
+  },
+  {
+    'value': '5',
+    'label': '人間科学部',
+  },
+  {
+    'value': '6',
+    'label': 'スポーツ科学部',
+  },
+  {
+    'value': '7',
+    'label': '国際教養学部',
+  },
+  {
+    'value': '8',
+    'label': '文化構想学部',
+  },
+  {
+    'value': '9',
+    'label': '文学部',
+  },
+  {
+    'value': '10',
+    'label': '基幹理工学部',
+  },
+  {
+    'value': '11',
+    'label': '創造理工学部',
+  },
+  {
+    'value': '12',
+    'label': '先進理工学部',
+  },
+];
+
+final List<Map<String, dynamic>> _items_3 = [
+  {
+    'value': '0',
+    'label': '教育学科',
+  },
+  {
+    'value': '1',
+    'label': '国語国文学科',
+  },
+  {
+    'value': '2',
+    'label': '英語英文学科',
+  },
+  {
+    'value': '3',
+    'label': '社会科',
+  },
+  {
+    'value': '4',
+    'label': '理学科',
+  },
+  {
+    'value': '5',
+    'label': '数学科',
+  },
+  {
+    'value': '6',
+    'label': '複合文化学科',
+  },
+];
+
 
 class _InformationPageState extends State<InformationPage> {
   @override
@@ -43,63 +135,157 @@ class _InformationPageState extends State<InformationPage> {
       appBar: AppBar(
         title: Text('seeeeeee'),
       ),
-      body: Column(
-        children:<Widget>[
-          Center(
-            child:Container(
-              height:100,
-              width:200,
-              child: Column(
-                children: <Widget>[
-                  Text('ニックネーム'),
-                  Padding(
-                    padding: const EdgeInsets.all(3.0),
-                    child: Container(
-                      child: TextFormField(),
-                      width: 100,
-                      height: 50,
-                      decoration: BoxDecoration(
-                        color: kMainColour,
-                        borderRadius: BorderRadius.circular(10),
+      body: SingleChildScrollView(
+        child: Column(
+          children:<Widget>[
+            Center(
+              child:Center(
+                child:Container(
+                  height:100,
+                  width:200,
+                  child: Column(
+                    children: <Widget>[
+                      Text('ニックネーム'),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ],
+                  ),
+                ) ,
+              ),
+            ),
+            Center(
+              child:Container(
+                height:100,
+                width:200,
+                child: Column(
+                  children: <Widget>[
+                    Text('アクセスキー'),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
                       ),
                     ),
+                  ],
+                ),
+              ) ,
+            ),
+            Center(
+              child:Center(
+                child:Container(
+                  height:100,
+                  width:200,
+                  child: Column(
+                    children: <Widget>[
+                      Text('学校の頭文字'),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
+                ) ,
               ),
-              color: Colors.white,
-            ) ,
-          ),
-          SelectFormField(
-            type: SelectFormFieldType.dropdown, // or can be dialog
-            initialValue: 'circle',
-            labelText: '大学',
-            items: _items,
-            onChanged: (val) => print(val),
-            onSaved: (val) => print(val),
-          ),
-          Center(
-            child:Container(
-              height:100,
-              width:200,
-              child:TextFormField(),
-              color: Colors.lightBlueAccent,
-            ) ,
-          ),
+            ),
+            SelectFormField(
+              type: SelectFormFieldType.dropdown, // or can be dialog
+              initialValue: 'circle',
+              labelText: '大学',
+              items: _items,
+              onChanged: (val) => print(val),
+              onSaved: (val) => print(val),
+            ),
+            SelectFormField(
+              type: SelectFormFieldType.dropdown, // or can be dialog
+              initialValue: 'circle',
+              labelText: '学部',
+              items: _items_2,
+              onChanged: (val) => print(val),
+              onSaved: (val) => print(val),
+            ),
 
+            SelectFormField(
+              type: SelectFormFieldType.dropdown, // or can be dialog
+              initialValue: 'circle',
+              labelText: '学科',
+              items: _items_3,
+              onChanged: (val) => print(val),
+              onSaved: (val) => print(val),
+            ),
 
+            //コースボタン
+            Text('コース選択'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children:<Widget>[
+                Container(
+                  child: RaisedButton(
+                    child: new Text('ライト'),
+                    textColor: Colors.white,
+                    color: button_1 ? Colors.blue : Colors.black,
+                    onPressed: () => {
+                      setState(() {
+                        button_1 = true;
+                      })
+                    },
+                  ),
+                ),
+                Container(
+                    child: RaisedButton(
+                      child: new Text('スタンダード'),
+                      textColor: Colors.white,
+                      color: button_2 ? Colors.blue : Colors.black,
+                      onPressed: () => {
+                        setState(() {
+                          button_2 = true;
+                        })
+                      },
+                    ),
+                ),
+                Container(
+                    child: RaisedButton(
+                      child: new Text('ヘヴィ'),
+                      textColor: Colors.white,
+                      color: button_3 ? Colors.blue : Colors.black,
+                      onPressed: () => {
+                        setState(() {
+                          button_3 = true;
+                        }),
+                      },
+                    ),
+                ),
+                Container(
+                    child: RaisedButton(
+                      child: new Text('エキスパート'),
+                      textColor: Colors.white,
+                      color: button_4 ? Colors.blue : Colors.black,
+                      onPressed: () => {
+                        setState(() {
+                          button_4 = true;
+                        }),
+                      },
+                    ),
+                ),
+              ],
+            ),
 
-          Align(
-            alignment: Alignment.bottomCenter,
-            child:ElevatedButton(
-              child: Text("MainPage"),
-              onPressed: (){
-                Navigator.push(
-                    context, MaterialPageRoute(builder: (context) => MainPage())
-                );
-              },
-            )
-          ),
-        ],
+            //メインページへ遷移
+            Align(
+                alignment: Alignment.bottomCenter,
+                child:ElevatedButton(
+                  child: Text("MainPage"),
+                  onPressed: (){
+                    Navigator.push(
+                        context, MaterialPageRoute(builder: (context) => MainPage())
+                    );
+                  },
+                )
+            ),
+          ],
+        ),
       ),
     );
   }
